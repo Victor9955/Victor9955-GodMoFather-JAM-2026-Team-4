6f5d4e7e7e829d34d71686d6bbaca23af4da0e5e
nonogui <nonogui00@gmail.com>
2025-09-03 16:56:20 +0200

    DotTween

--- Assets/00_Scripts/Timer.cs
+++ Assets/00_Scripts/Timer.cs
@@ -3,14 +3,20 @@ using UnityEngine.UI;
 using UnityEngine.SocialPlatforms.Impl;
 using TMPro;
 using System.Collections.Generic;
+using DG.Tweening;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField]private TextMeshProUGUI timerText;
-    [SerializeField]private float gameTime;
-    [SerializeField]private AudioClip audioClip = null;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI timerFin;
+    [SerializeField] private GameObject timerFinObj;
+    [SerializeField] private GameObject timerTextObj;
+
+    [SerializeField] private float gameTime;
+    [SerializeField] private AudioClip audioClip = null;
     private bool isStopTimer;
     private AudioSource audioSource;
+
 
     private void Awake()
     {
@@ -22,6 +28,13 @@ public class Timer : MonoBehaviour
 
 
     }
+    public void StarSon()
+    {
+        if (audioClip != null)
+        {
+           audioSource.PlayOneShot(audioClip);
+        }
+    }
 
     void Update()
     {
@@ -32,15 +45,29 @@ public class Timer : MonoBehaviour
             int seconds = Mathf.FloorToInt(gameTime - minutes * 60);
             int milliseconds = Mathf.FloorToInt((gameTime * 100F) % 100);
             timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
-            if (gameTime <= 10 && audioClip != null && !audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(audioClip);
-            }
+            timerFin.text = gameTime.ToString("0");
+
         }
-        if (gameTime <= 0)
+
+        int gameTimeInt = (int)gameTime;
+        if (gameTimeInt <= 5 && audioClip != null && !audioSource.isPlaying)
         {
-            isStopTimer = true;
-            timerText.text = "00:00:00";
+            StarSon();
+        }
+        
+
+        if (gameTimeInt < 5)
+        {
+            timerFin.color = Color.red;
+            timerFinObj.SetActive(true);
+            timerTextObj.SetActive(false);
+            timerFinObj.transform.DOScale(1f, 3f);
+            if (gameTime <= 0)
+            {
+                isStopTimer = true;
+                timerText.text = "00:00:00";
+                timerFin.text = "FIN";
+            }
         }
     }
 }
